1df2326f2fa174104de8c9fdf6ee807b64460a25
huoluan <995427616@qq.com>
2024-10-23 18:45:02 +0800

    icon向addressable适配

--- Assets/DataBase/DataLoad.cs
+++ Assets/DataBase/DataLoad.cs
@@ -12,12 +12,12 @@ public class DataLoad : MonoBehaviour
     public MainBuildingSOPage mainBuildingSOPage;
     public OtherBuildingSOPage otherBuildingSOPage;
     public ArmySOPage armySOPage;
-
     public string excelFilePath;
     public string armorTypePath;
+    public string damageTypePath;
     public string armyLabelPath;
     public string buildingLabelPath;
-    public string damageTypePath;
+
 
     public string factionDBPath;
 
@@ -34,57 +34,45 @@ public class DataLoad : MonoBehaviour
     [ContextMenu("LoadArmorData")]
     public void LoadArmorData()
     {
-
         string folderPath = armorTypePath;
         var existedFileName = GetFileNameInPath(folderPath);
-        FileStream fileStream = new FileStream(excelFilePath, FileMode.Open, FileAccess.Read);
-        var workbook =  new XSSFWorkbook(fileStream);
-        ISheet sheet = workbook.GetSheetAt(3);
-
-        var damageTypeSOs = GetDamageTypeSOs();
-
-        for(int i = 1; i <= 35; i++)
+        using (FileStream fileStream = new FileStream(excelFilePath, FileMode.Open, FileAccess.Read))
         {
-
-            var currentRow = sheet.GetRow(i);
-            int index = int.Parse(currentRow.GetCell(0).ToString());
-            string armorNameZH = currentRow.GetCell(1).ToString();
-            string armorNameEN = currentRow.GetCell(2).ToString();
-            string fileName = $"{index}_{armorNameEN}.asset";
-            string filePath = "Assets" + folderPath + "/" + fileName;
-            if(existedFileName.Contains(fileName))
+            var workbook = new XSSFWorkbook(fileStream);
+            ISheet sheet = workbook.GetSheetAt(3);
+            var damageTypeSOs = GetDamageTypeSOs();
+            for (int i = 1; i <= 35; i++)
             {
-                ArmorSO armorSO = AssetDatabase.LoadAssetAtPath<ArmorSO>(filePath);
+                var currentRow = sheet.GetRow(i);
+                int index = int.Parse(currentRow.GetCell(0).ToString());
+                string armorNameZH = currentRow.GetCell(1).ToString();
+                string armorNameEN = currentRow.GetCell(2).ToString();
+                string fileName = $"{index}_{armorNameEN}.asset";
+                string filePath = "Assets" + folderPath + "/" + fileName;
+                Action loadCallBack;
+                ArmorSO armorSO;
+                if (existedFileName.Contains(fileName))
+                {
+                    armorSO = AssetDatabase.LoadAssetAtPath<ArmorSO>(filePath);
+                    loadCallBack = new Action(() => Debug.Log(armorNameZH + "---同步完成"));
+                }
+                else
+                {
+                    armorSO = ScriptableObject.CreateInstance<ArmorSO>();
+                    AssetDatabase.CreateAsset(armorSO, filePath);
+                    loadCallBack = new Action(() => Debug.Log(armorNameZH + "---创建并同步完成"));
+                }
                 armorSO.SetIndex(index);
                 armorSO.SetArmorNameZH(armorNameZH);
                 armorSO.SetArmorNameEN(armorNameEN);
-                for(int j = 1; j <= 15; j++)
+                for (int j = 1; j <= 15; j++)
                 {
                     int value = int.Parse(currentRow.GetCell(j + 3).ToString());
                     armorSO.SetDamageModifiers(damageTypeSOs[j], value);
                 }
-                Debug.Log(armorNameZH + "---同步完成");
+                loadCallBack.Invoke();
             }
-            else
-            {
-                ArmorSO armorSO = ScriptableObject.CreateInstance<ArmorSO>();
-                AssetDatabase.CreateAsset(armorSO, filePath);
-                armorSO.SetIndex(index);
-                armorSO.SetArmorNameZH(armorNameZH);
-                armorSO.SetArmorNameEN(armorNameEN);
-                for(int j = 1; j <= 15; j++)
-                {
-                    int value = int.Parse(currentRow.GetCell(j + 3).ToString());
-                    armorSO.SetDamageModifiers(damageTypeSOs[j], value);
-                }
-                Debug.Log(armorNameZH + "---创建并同步完成");
-            }
-            
         }
-    }
-    public void UpData()
-    {
-
     }
 
     private List<string> GetFileNameInPath(string pathName)
@@ -115,9 +103,12 @@ public class DataLoad : MonoBehaviour
     [ContextMenu("DownloadExcel")]
     void LoadExcelAsync()
     {
-        
-    }
 
+    }
+    public class FactionSOPath
+    {
+
+    }
 
 
 }
